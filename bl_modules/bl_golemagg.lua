2bf21b6aeffc231aba7e5e25178c8ce3ff6db79c
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-08-27 06:37:45 +0100

    Added function to dynamically update cbl player info

--- bl_modules/bl_golemagg.lua
+++ bl_modules/bl_golemagg.lua
@@ -3,11 +3,11 @@ CBL = LibStub("AceAddon-3.0"):GetAddon("ClassicBlacklist")
 
 -- The central Golemagg discord blocklist.
 CBL.bl_golemagg = {
-    ["Korstoget"] = {
+    ["Killerzenn"] = {
         reason = "Test: is a dirty rotten no good cheat.",
         evidence = "https://discord.com/channels/610036506974748700/1007955249266425946/1007984013555810354",
     },
-    ["Nexttra"] = {
+    ["Playerb"] = {
         reason = "Test: stole all the gold from a GDKP.",
         evidence = "https://discord.com/channels/610036506974748700/1011016570467790979/1011657792651788289",
     },
