d0fdd2255f1ff0d0d8aad367c55695c6bcf1be25
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-08-27 07:20:11 +0100

    Fixed dynamic updates to cbl individuals

--- bl_modules/bl_golemagg.lua
+++ bl_modules/bl_golemagg.lua
@@ -3,11 +3,7 @@ CBL = LibStub("AceAddon-3.0"):GetAddon("ClassicBlacklist")
 
 -- The central Golemagg discord blocklist.
 CBL.bl_golemagg = {
-    ["Killerzenn"] = {
-        reason = "Test: is a dirty rotten no good cheat.",
-        evidence = "https://discord.com/channels/610036506974748700/1007955249266425946/1007984013555810354",
-    },
-    ["Playerb"] = {
+    ["Caaps"] = {
         reason = "Test: stole all the gold from a GDKP.",
         evidence = "https://discord.com/channels/610036506974748700/1011016570467790979/1011657792651788289",
     },
