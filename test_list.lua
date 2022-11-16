79006add4739b9965fe2e696d17e8087baf8c110
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-11-16 01:58:27 +0000

    Beginning of widescale rework
    Changes to:
    - how lists are handled
    - how Cutpurse is extended
    - the structure of scanning and unit checking

---
+++ test_list.lua
@@ -0,0 +1,20 @@
+-- This module contains a test list for debugging purposes.
+
+local bl = {
+    provider = "Golemagg EU",
+    description = "Golemagg EU Discord",
+    url = "some_url",
+    data = {
+        Golemagg = {
+            [0] = {
+                last_known_name = "TestName",
+                last_known_guid = "Player-GDSAKG-53295G",
+                reason = "Raid Scame",
+                evidence = "some_other_url",
+                previous_aliases = {
+                    SomeOtherName = "Player-GNFDSA-2153FSA",
+                },
+            },
+        }
+    }
+}
