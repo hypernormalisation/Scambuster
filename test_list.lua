d8e8248307382f7b79ce7fc1f949251d6b80a712
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-11-17 03:15:13 +0000

    Added code to build curated list local and global db from registered lists

--- test_list.lua
+++ test_list.lua
@@ -2,12 +2,11 @@ local addon_name, cp = ...
 if not cp.add_test_list then return end
 
 -- This module contains a test list for debugging purposes.
-
 local golemagg_list = {
     [0] = {
         last_known_name = "TestName",
         last_known_guid = "Player-GDSAKG-53295G",
-        reason = "Raid Scame",
+        reason = "Raid Scam",
         evidence = "some_other_url",
         previous_aliases = {
             SomeOtherName = "Player-GNFDSA-2153FSA",
@@ -16,29 +15,18 @@ local golemagg_list = {
 }
 
 local my_test_bl = {
+    name = "Golemagg EU Discord Blocklist",
     provider = "Golemagg EU",
     description = "Golemagg EU Discord",
     url = "some_url",
-    data = {
+    realm_data = {
         Golemagg = golemagg_list
     }
 }
 
 
-local CP = LibStub("AceAddon-3.0"):GetAddon("CutPurse")
+local CP = LibStub("AceAddon-3.0"):GetAddon("Cutpurse")
 CP.RegisterCallback(
     CP, "CUTPURSE_LIST_CONSTRUCTION",
     function() CP:register_curated_list(my_test_bl) end
 )
-
--- local f = CreateFrame("Frame", nil)
--- f:RegisterEvent("ADDON_LOADED")
--- local load_list = function(self, event, addon_name)
---     if addon_name ~= "CutPurse" then return end
---     CP = LibStub("AceAddon-3.0"):GetAddon("CutPurse")
---     CP:register_curated_list(my_test_bl)
---     f:UnregisterAllEvents()
---     f:SetScript("OnEvent", nil)
--- end
-
--- f:SetScript("OnEvent", load_list)
