136982d510a08c67335e93e64f74c31d99e36265
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-11-21 02:59:40 +0000

    Fix to previous alias parsing

--- test_list.lua
+++ test_list.lua
@@ -4,17 +4,22 @@ if not cp.add_test_list then return end
 -- This module contains a test list for debugging purposes.
 local golemagg_list = {
     [0] = {
-        last_known_name = "TestName",
+        last_known_name = "Thrall",
         last_known_guid = "Player-GDSAKG-53295G",
         reason = "Raid Scam",
         evidence = "some_other_url",
         previous_aliases = {
-            SomeOtherName = "Player-GNFDSA-2153FSA",
+            Raegar = "Player-GNFDSA-2153FSA",
         },
-    }
+    },
+    [1] = {
+        last_known_name = "Arthas",
+        last_known_guid = "Player-GDDFDG-535321",
+        reason = "Dungeon Scam",
+        evidence = "some_other_url_again",
+    },
 }
-
-local my_test_bl = {
+local test_bl_1 = {
     name = "Golemagg EU Discord Blocklist",
     provider = "Golemagg EU",
     description = "Golemagg EU Discord",
@@ -24,9 +29,44 @@ local my_test_bl = {
     }
 }
 
+-- Now another list with coincidences to test case-building
+local golemagg_list2 = {
+    [0] = {
+        last_known_name = "Arthas",
+        last_known_guid = "Player-GDDFDG-535321",
+        reason = "Trade Scam",
+        evidence = "some_other_url_yet_again",
+    },
+    [1] = {
+        last_known_name = "Thrall",
+        last_known_guid = "Player-GDSAKG-53295G",
+        reason = "Raid Scam",
+        evidence = "some_other_url",
+        previous_aliases = {
+            Durotan = "Player-GJNGDS-2532FHG",
+        },
+    }
+}
+
+local test_bl_2 = {
+    name = "Orcs Anonymous Blocklist",
+    provider = "ZugZug",
+    description = "List of orcs who didn't zug.",
+    url = "zug_url",
+    realm_data = {
+        Golemagg = golemagg_list2
+    }
+}
 
 local CP = LibStub("AceAddon-3.0"):GetAddon("Cutpurse")
 CP.RegisterCallback(
     CP, "CUTPURSE_LIST_CONSTRUCTION",
-    function() CP:register_curated_list(my_test_bl) end
+    function()
+        CP:register_curated_list(test_bl_1)
+        CP:register_curated_list(test_bl_2)
+    end
 )
+-- CP.RegisterCallback(
+--     CP, "CUTPURSE_LIST_CONSTRUCTION",
+--     function() CP:register_curated_list(test_bl_2) end
+-- )
