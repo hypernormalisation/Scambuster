9d28fec29304a6e67ffd0bdee622c9e156ddfdd2
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-12-18 21:37:04 +0000

    Refactor to db, pdi is now udi, unit dynamic info

--- locales/frFR.lua
+++ locales/frFR.lua
@@ -2,9 +2,9 @@ local _, cp = ...
 
 if GetLocale() ~= "frFR" then return end
 
-local L = {
-    ["has requested to join your group"] = "a demandé à rejoindre votre group",
-}
+-- local L = {
+--     ["has requested to join your group"] = "a demandé à rejoindre votre group",
+-- }
 
-cp.L = L
-cp.L = setmetatable(L, {__index = cp.L})
+-- cp.L = L
+-- cp.L = setmetatable(L, {__index = cp.L})
