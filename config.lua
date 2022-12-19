e0fbd30b86cbb6066bc5e91c7359cbd505984c7e
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-12-19 03:53:24 +0000

    Added more config options for alerts and scans

--- config.lua
+++ config.lua
@@ -18,18 +18,21 @@ CP.defaults = {
 		n_alerts = 0,
 	},
 	profile = {
+		-- general
 		welcome_message = true,
-        -- alert settings
+
+		-- alert settings
         use_chat_alert = true,
 		use_gui_alert = true,
-		b_play_alert_sound = true,
+		use_alert_sound = true,
 		alert_sound = "Cutpurse: Criminal Scum!",
-
         grace_period_s = 10,
+
 		-- Scanning settings
 		use_mouseover_scan = true,
 		use_whisper_scan = true,
 		use_target_scan = true,
+		use_group_scan = true,
 		use_group_request_scan = true,
 		alert_lockout_seconds = 900,
     },
@@ -49,16 +52,7 @@ CP.options = {
 			get = "opts_getter",
 			set = "opts_setter",
 		},
-		alert_sound = {
-			order = 2,
-			type = "select",
-			name = "Sound Alert",
-			desc = "The sound to play when a scammer is detected.",
-			dialogControl = "LSM30_Sound",
-			values = LSM:HashTable("sound"),
-			get = "opts_getter",
-			set = "opts_setter",
-		},
+
 
 		-- Scanning settings
 		scan_header = {
@@ -69,14 +63,14 @@ CP.options = {
 		scan_desc = {
 			order = 3.01,
 			type = "description",
-			name = "Cutpurse can detect your interactions with scammers by running various scans on players you interact with."..
+			name = "Cutpurse can protect you from scammers by running various scans on players you interact with."..
 				" This section allows you to control in what ways Cutpurse will do this."
 		},
 		use_mouseover_scan = {
 			order = 3.2,
 			type = "toggle",
 			name = "Mouseover",
-			desc = "If enabled, will check any mouseover players against the blocklists.",
+			desc = "If enabled, will check any mouseover players against the database.",
 			get = "opts_getter",
 			set = function(_, value)
 				CP.db.profile.use_mouseover_scan = value
@@ -90,7 +84,7 @@ CP.options = {
 			order = 3.3,
 			type = "toggle",
 			name = "Whispers",
-			desc = "If enabled, will check any players whispering you against the blocklists.",
+			desc = "If enabled, will check any players whispering you against the database.",
 			get = "opts_getter",
 			set = function(_, value)
 				CP.db.profile.use_whisper_scan = value
@@ -116,9 +110,45 @@ CP.options = {
 				end
 			end
 		},
+		use_group_scan = {
+			order = 3.5,
+			type = "toggle",
+			name = "Party/Raid",
+			desc = "If enabled, will check any players in your party or raid agaist the database.",
+			get = "opts_getter",
+			set = function(_, value)
+				CP.db.profile.use_group_scan = value
+				if value then
+					CP:RegisterEvent("GROUP_ROSTER_UPDATE")
+				else
+					CP:UnregisterEvent("GROUP_ROSTER_UPDATE")
+				end
+			end
+		},
+		use_group_request_scan = {
+			order = 3.6,
+			type = "toggle",
+			name = "Invite Confirmations",
+			desc = "If enabled, will check any players suggested to join your party or raid by another "..
+				"group member or via the group finder tool.",
+			get = "opts_getter",
+			set = function(_, value)
+				CP.db.profile.use_group_request_scan = value
+				if value then
+					CP:RegisterEvent("GROUP_INVITE_CONFIRMATION")
+				else
+					CP:UnregisterEvent("GROUP_INVITE_CONFIRMATION")
+				end
+			end
+		},
+		lb1 = {
+			order = 3.9,
+			type = "description",
+			name = "To avoid spam, Cutpurse will only generate warnings for a given scammer once per a lockout period, configurable below.",
+		},
 
 		alert_lockout_seconds = {
-			order = 4.0,
+			order = 3.91,
 			type = "range",
 			name = "Alert Lockout (s)",
 			desc = "The period during which the addon will not generate alerts for "..
@@ -132,6 +162,43 @@ CP.options = {
 			set = "opts_setter",
 		},
 
+		alerts_header = {
+			order = 4.0,
+			type = "header",
+			name = "Alerts Behaviour",
+		},
+		alerts_desc = {
+			order = 4.1,
+			type = "description",
+			name = "Cutpurse can alert you when you encounter a scammer in a number of ways."
+		},
+		use_chat_alert = {
+			order = 4.2,
+			type = "toggle",
+			name = "Chat panel",
+			desc = "If enabled, Cutpurse will print a summary of the scanner's information to the chat panel when an alert is raised.",
+			get = "opts_getter",
+			set = "opts_setter,"
+		},
+		use_alert_sound = {
+			order = 4.3,
+			type = "toggle",
+			name = "Audio Alert",
+			desc = "If enabled, Cutpurse will play an audio cue when an alert is raised.",
+			get = "opts_getter",
+			set = "opts_setter",
+		},
+		alert_sound = {
+			order = 4.4,
+			type = "select",
+			name = "Sound Alert",
+			desc = "The sound to play when a scammer is detected.",
+			dialogControl = "LSM30_Sound",
+			values = LSM:HashTable("sound"),
+			get = "opts_getter",
+			set = "opts_setter",
+			disabled = function() return not CP.db.profile.use_alert_sound end,
+		},
 	}
 }
 
