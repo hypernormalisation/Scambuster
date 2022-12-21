25f3c56a04bf1b991810ef5524167a8fab94c7af
hypernormalisation <52763122+hypernormalisation@users.noreply.github.com>
2022-12-21 05:26:06 +0000

    Beginning of move to relational database

--- test_list.lua
+++ test_list.lua
@@ -4,69 +4,62 @@
 local addon_name, cp = ...
 if not cp.add_test_list then return end
 
-local golemagg_list = {
+local case_data_1 = {
     [0] = {
-        last_known_name = "Thrall",
-        last_known_guid = "Player-GDSAKG-53295G",
+        name = "Thrall",
+        guid = "Player-GDSAKG-53295G",
         reason = "Raid Scam",
+        description = "Stole all the gold from a gdkp.",
         evidence = "some_other_url",
-        previous_aliases = {
-            Raegar = "Player-GNFDSA-2153FSA",
-        },
+        aliases = {"Raegar"},
     },
     [1] = {
-        last_known_name = "Arthas",
-        last_known_guid = "Player-GDDFDG-535321",
+        name = "Arthas",
+        guid = "Player-GDDFDG-535321",
         reason = "Dungeon Scam",
+        description = "Ninja needed an item they could not use.",
         evidence = "some_other_url_again",
     },
-}
-
-local ptr_list_1 = {
     [2] = {
-        last_known_name = "Swedger",
-        last_known_guid = "Player-4904-0079C620",
-        -- last_known_guid = "SOME-WRONG-GUID",
+        name = "Swedger",
+        guid = "Player-4904-0079C620",
+        --guid = "SOME-WRONG-GUID",
         reason = "Raid Scam",
+        description = "",
         evidence = "some_other_url",
     },
 }
-for k, v in pairs(golemagg_list) do ptr_list_1[k] = v end
-
 local test_bl_1 = {
     name = "Golemagg EU Discord Blocklist",
     provider = "Golemagg EU Discord",
     description = "Realm discord for the Golemagg EU realm.",
     url = "some_url",
     realm_data = {
-        Golemagg = golemagg_list,
-        ["Classic PTR Realm 1"] = ptr_list_1,
+        Golemagg = case_data_1,
+        ["Classic PTR Realm 1"] = case_data_1,
     }
 }
 
--- Now another list with coincidences to test case-building
-local golemagg_list2 = {
+-- Now another list with coincidences to test cross-list case-building
+local case_data_2 = {
     [0] = {
-        last_known_name = "Arthas",
-        last_known_guid = "Player-GDDFDG-535321",
+        name = "Arthas",
+        guid = "Player-GDDFDG-535321",
         reason = "Trade Scam",
         evidence = "some_other_url_yet_again",
     },
     [1] = {
-        last_known_name = "Thrall",
-        last_known_guid = "Player-GDSAKG-53295G",
+        name = "Thrall",
+        guid = "Player-GDSAKG-53295G",
         reason = "Raid Scam",
         evidence = "some_other_url",
-        previous_aliases = {
-            Durotan = "Player-GJNGDS-2532FHG",
-        },
+        aliases = {"Durotan"},
+        previous_guids = {"Player-GJNGDS-2532FHG"},
     },
     [2] = {
-        last_known_name = "Swedger",
+        name = "Swedger",
         reason = "Dungeon Scam",
         evidence = "url_to_evidence_here.com",
-        -- last_known_guid = "another-wrong-guid",
-        -- last_known_guid = "Player-4904-0079C620",
     }
 }
 local test_bl_2 = {
@@ -75,8 +68,8 @@ local test_bl_2 = {
     description = "List of orcs who didn't zug.",
     url = "zug_url",
     realm_data = {
-        Golemagg = golemagg_list2,
-        ["Classic PTR Realm 1"] = golemagg_list,
+        Golemagg = case_data_2,
+        ["Classic PTR Realm 1"] = case_data_2,
     }
 }
 
